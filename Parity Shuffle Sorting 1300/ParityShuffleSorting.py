3b2ac5715d7df3d0fdd8c603427ded6eb4625d18
HRyohni <matosevic.leo@gmail.com>
2022-10-18 13:33:54 +0200

    Update ParityShuffleSorting.py

--- Parity Shuffle Sorting 1300/ParityShuffleSorting.py
+++ Parity Shuffle Sorting 1300/ParityShuffleSorting.py
@@ -1,22 +1,33 @@
-# broj testova
-brTestova=int(input())
 listaDone=[]
-for x in range(brTestova):
-    input()
-    lista = list (map(int, input().split()))
-    for y in range(len(lista)-1):
-        if lista[y]>lista[y+1]:
-            if (lista[y] + lista[y+1])% 2 == 0: #paran
-                lista[y+1]=lista[y]
-                listaDone.append(y)
-                listaDone.append(y+1)
-            else: #neparan
-                lista[y]=lista[y+1]
-                listaDone.append(y+1)
-                listaDone.append(y)
-        print(lista)
-                
-    print(listaDone)
+def math(lista):
+    while ((sorted(lista)==lista) == False): # bolji uvijet
+          for y in range(len(lista)-1):
+                if lista[y]>lista[y+1]:
+                    if (lista[y] + lista[y+1])% 2 == 0: #paran
+                        lista[y+1]=lista[y]
+                        listaDone.append(y)
+                        listaDone.append(y+1)
+                        print(y ,y+1,"\n")
+                    else: #neparan
+                        lista[y]=lista[y+1]
+                        listaDone.append(y+1)
+                        listaDone.append(y)
+                        print(y+1 ,y,"\n")
     
+    
+    print("krajna lista: ",*lista)
+    print("brZamjena",int(len(listaDone)/2))
+                  
+brTestova=int(input())
+for x in range(brTestova):        
+    VelicinaPolja= int(input())
+    lista = list (map(int, input().split()))
+
+math(lista)
 
 
+
+
+
+
+    
